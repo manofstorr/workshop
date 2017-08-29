f825d35c73826645589e4975029dbe2b2b422c25
manofstorr <david.baudry@zoho.com>
2017-08-29 16:27:52 +0200

    Rabbit functionnal Index page ok

--- rabbit-consummer/simpleconsummer1.php
+++ rabbit-consummer/simpleconsummer1.php
@@ -1,6 +1,5 @@
 <?php
-
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
@@ -8,11 +7,11 @@ $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 $channel->queue_declare('hello', false, false, false, false);
 echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
-$callback = function($msg) {
+$callback = function ($msg) {
     echo " [x] Received ", $msg->body, "\n";
 };
 $channel->basic_consume('hello', '', false, true, false, false, $callback);
-while(count($channel->callbacks)) {
+while (count($channel->callbacks)) {
     $channel->wait();
 }
 $channel->close();
--- web/index.php
+++ web/index.php
@@ -1,10 +1,31 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: d.baudry
- * Date: 28/08/2017
- * Time: 10:55
- */
+<!DOCTYPE html>
+<html>
 
-echo '<h1>Workshop</h1>';
-echo '<img src="static/img/rocket.jpg" alt="rocket" />';
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Workshop</title>
+    <link rel="stylesheet" href="static/css/bootstrap.css" type="text/css"/>
+</head>
+
+<body>
+<?php include_once 'webpagecomponents/navbarcomponent.php'; ?>
+<div class="container">
+
+    // Content here
+    <br />
+    <hr />
+    <img src="static/img/rocket.jpg" alt="rocket"/>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <address>Contact : davyyd76@gmail.com</address>
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</body>
+</html>
